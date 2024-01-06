4b1cfbe5d6c4e8f4e1959d8bbba6f2dc6bb0d8e1
vaibhavasharma4395 <155812960+vaibhavasharma4395@users.noreply.github.com>
2024-01-06 16:36:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,5 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacodo toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
