036cb3812f919687190465acd22a96c45ded13cf
vaibhavasharma4395 <155812960+vaibhavasharma4395@users.noreply.github.com>
2024-01-06 16:37:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-
+streamlit.title('Family Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
